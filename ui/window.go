6b12c234e3b7c65814cd319f5556424121abfa92
Alina Dubchak <alina.dubchak.ev@gmail.com>
2023-04-27 15:49:32 +0300

    added '+' shape and mouse click reaction

--- ui/window.go
+++ ui/window.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"log"
@@ -25,15 +26,17 @@ type Visualizer struct {
 	tx   chan screen.Texture
 	done chan struct{}
 
-	sz  size.Event
-	pos image.Rectangle
+	sz       size.Event
+	pos      image.Rectangle
+	mousePos image.Point
 }
 
 func (pw *Visualizer) Main() {
 	pw.tx = make(chan screen.Texture)
 	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
+	pw.pos.Max.X = 100
+	pw.pos.Max.Y = 100
+	pw.mousePos = image.Point{X: 400, Y: 400}
 	driver.Main(pw.run)
 }
 
@@ -47,7 +50,9 @@ func (pw *Visualizer) run(s screen.Screen) {
 	}
 
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -115,7 +120,15 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 	case mouse.Event:
 		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
+			if e.Button == mouse.ButtonLeft && e.Direction == mouse.DirPress { // TODO: Реалізувати реакцію на натискання кнопки миші.
+				pw.mousePos = image.Point{
+					X: int(e.X),
+					Y: int(e.Y),
+				}
+
+				pw.w.Send(paint.Event{})
+				fmt.Println("X pos, Y pos: ", e.X, e.Y)
+			}
 		}
 
 	case paint.Event:
@@ -133,7 +146,17 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
 
+	x, y := pw.mousePos.X, pw.mousePos.Y
+	c := color.RGBA{R: 255, G: 255, B: 0, A: 1}
+    rectX := image.Rect(x-100, y+50, x+100, y-50)
+	rectY := image.Rect(x-50, y-100, x+50, y+100)
+	
+	pw.w.Fill(rectX, c, draw.Src)
+	pw.w.Fill(rectY, c, draw.Src)
+
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	//pw.w.Fill(image.Rect(250, 300, 550, 400), color.RGBA{R: 255, G: 255, B: 0, A: 1}, draw.Src)
+	//pw.w.Fill(image.Rect(350, 200, 450, 500), color.RGBA{R: 255, G: 255, B: 0, A: 1}, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
