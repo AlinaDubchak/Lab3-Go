30d40fab6e71dd0b7e990881ceb7139dc2137fe0
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-05-23 23:15:35 +0300

    fixed remarks
    - added implementation of StopAndWait in Loop
    - added cases for sync and race avoidance in MessageQueue
    - implemented blocking in Pull method when the queue is empty
    - made some styling corrections

--- painter/loop.go
+++ painter/loop.go
@@ -2,6 +2,7 @@ package painter
 
 import (
 	"image"
+	"sync"
 
 	"golang.org/x/exp/shiny/screen"
 )
@@ -19,6 +20,8 @@ type Loop struct {
 	prev screen.Texture // текстура, яка була відправленя останнього разу у Receiver
 
 	Mq messageQueue
+	Done chan struct{}
+	Stopped bool
 }
 
 var size = image.Pt(800, 800)
@@ -32,40 +35,71 @@ func (l *Loop) Start(s screen.Screen) {
 }
 
 func (l *Loop) eventProcessing() {
-	for {
-		if op := l.Mq.pull(); op != nil {
-			if update := op.Do(l.next); update {
+	for !l.Stopped || !l.Mq.isEmpty() {
+		op := l.Mq.Pull() 
+		update := op.Do(l.next)
+			if update {
 				l.Receiver.Update(l.next)
 				l.next, l.prev = l.prev, l.next
 			}
 		}
+		close(l.Done)
 	}
-}
 
 // Post додає нову операцію у внутрішню чергу.
 func (l *Loop) Post(op Operation) {
 	if op != nil {
-		l.Mq.push(op)
+		l.Mq.Push(op)
 	}
 }
 
 // StopAndWait сигналізує
 func (l *Loop) StopAndWait() {
+	l.Post(OperationFunc(func(t screen.Texture) {
+		l.Stopped = true
+	}))
+	l.Stopped = true
+	<-l.Done
 }
 
 type messageQueue struct {
 	Queue []Operation
+	mu         sync.Mutex
+	blocked    chan struct{}
 }
 
-func (Mq *messageQueue) push(op Operation) {
+func (Mq *messageQueue) Push(op Operation) {
+	Mq.mu.Lock()
+	defer Mq.mu.Unlock()
+
 	Mq.Queue = append(Mq.Queue, op)
+
+	if Mq.blocked != nil {
+		close(Mq.blocked)
+		Mq.blocked = nil
+	}
 }
 
-func (Mq *messageQueue) pull() Operation {
-	if len(Mq.Queue) == 0 {
-		return nil
+func (Mq *messageQueue) Pull() Operation {
+	Mq.mu.Lock()
+	defer Mq.mu.Unlock()
+
+	for len(Mq.Queue) == 0 {
+		Mq.blocked = make(chan struct{})
+		Mq.mu.Unlock()
+		<-Mq.blocked
+		Mq.mu.Lock()
 	}
+
 	op := Mq.Queue[0]
+	Mq.Queue[0] = nil
 	Mq.Queue = Mq.Queue[1:]
 	return op
+}
+
+func (Mq *messageQueue) isEmpty() bool {
+	Mq.mu.Lock()
+	defer Mq.mu.Unlock()
+
+	return len(Mq.Queue) == 0
 }
